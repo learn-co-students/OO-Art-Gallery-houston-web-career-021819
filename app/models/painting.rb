a4ddf1a8941bda2a785133751e2cab1c6748e32d
sm00th <ezzybrzy@sm00ths-MacBook-Air.local>
2019-02-27 16:41:04 -0600

    done

--- app/models/painting.rb
+++ app/models/painting.rb
@@ -7,6 +7,16 @@ class Painting
   def initialize(title, price)
     @title = title
     @price = price
+    @@all << self
+  end
+
+  def self.all 
+    @@all
+  end
+
+  def self.total_price
+    price_array = self.all.price
+    price_array.inject{|sum, x| sum + x}
   end
 
 end
